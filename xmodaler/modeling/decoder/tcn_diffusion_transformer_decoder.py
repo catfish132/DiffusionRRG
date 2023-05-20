a1e57ffcbd10fb5c6de6193157816e3ae39937e4
Catfish132 <1007091103@qq.com>
2023-05-20 10:18:28 +0800

    实现了cascade

--- xmodaler/modeling/decoder/tcn_diffusion_transformer_decoder.py
+++ xmodaler/modeling/decoder/tcn_diffusion_transformer_decoder.py
@@ -11,7 +11,7 @@ from xmodaler.config import CfgNode as CN
 from xmodaler.config import kfg
 from .transformer_decoder import TransformerDecoder
 from .build import DECODER_REGISTRY
-from ..layers.bert import TcnBertGenerationLayer
+from ..layers.bert import TcnBertGenerationLayer, BertGenerationLayer, TCN
 
 __all__ = ["TcnDiffusionTransformerDecoder"]
 
@@ -23,21 +23,24 @@ class TcnDiffusionTransformerDecoder(TransformerDecoder):
             self,
             *,
             num_generation_layers: int,
-            bert_generation_layers
+            bert_generation_layers,
+            tcn
     ):
         super(TcnDiffusionTransformerDecoder, self).__init__(
             num_generation_layers=num_generation_layers,
             bert_generation_layers=bert_generation_layers
         )
+        self.tcn = tcn
 
     @classmethod
     def from_config(cls, cfg):
         bert_generation_layers = nn.ModuleList(
-            [TcnBertGenerationLayer(cfg) for _ in range(cfg.MODEL.BERT.NUM_GENERATION_LAYERS)]
+            [BertGenerationLayer(cfg) for _ in range(cfg.MODEL.BERT.NUM_GENERATION_LAYERS)]
         )
         return {
             "num_generation_layers": cfg.MODEL.BERT.NUM_GENERATION_LAYERS,
             "bert_generation_layers": bert_generation_layers,
+            "tcn": TCN(cfg)
         }
 
     def forward(self, batched_inputs):
@@ -45,13 +48,15 @@ class TcnDiffusionTransformerDecoder(TransformerDecoder):
         vfeats = batched_inputs[kfg.ATT_FEATS]
         ext_vmasks = batched_inputs[kfg.EXT_ATT_MASKS]
 
-        u_tfeats_arr = []
+        u_tfeats_arr = []  # 只有最后一个是有效的
         u_tfeats = batched_inputs[kfg.U_TOKEN_EMBED]
         ext_u_tmasks = batched_inputs[kfg.EXT_U_TOKENS_MASKS]
 
         for i, layer_module in enumerate(self.g_layers):
             u_tfeats = layer_module(u_tfeats, vfeats, ext_u_tmasks, ext_vmasks, t_history_states=None)
             u_tfeats_arr.append(u_tfeats)
+        u_tfeats = self.tcn(u_tfeats)
+        u_tfeats_arr.append(u_tfeats)
         ret.update({kfg.U_HIDDEN_STATES: u_tfeats_arr})
 
         return ret
